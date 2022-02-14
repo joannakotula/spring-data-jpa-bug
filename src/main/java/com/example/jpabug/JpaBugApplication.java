c20997fff7006a4d6b5c70bffe5563cf7c9f23af
Joanna Kotuła <joanna@kotula.net.pl>
2022-02-14 13:54:14 +0100

    From Spring Initializr

---
+++ src/main/java/com/example/jpabug/JpaBugApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpabug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpabug/JpaBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpabug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
